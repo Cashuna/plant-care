c73a3dfd1bfd6357f47be202f0e5d9c6ac5049c9
Nicole <nmarielockwood@gmail.com>
2017-05-13 21:49:32 -0400

    adding plant care data and editing Plants schema

--- models/plant.js
+++ models/plant.js
@@ -6,51 +6,82 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false
 		},
+		plant_name_sci: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
+		},
+		plant_type: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
+		},
+		origin: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
+		},
 		lifecycle: {
-			type: DataTypes.STRING
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		mature_height: {
-			type: DataTypes.DECIMAL
+		plant_shape: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		mature_spread: {
-			type: DataTypes.DECIMAL
+		frost_hardiness: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		water: {
-			type: DataTypes.STRING
+		mature_ht_val: {
+			type: DataTypes.DECIMAL(10,1)
 		},
-		sunlight: {
-			type: DataTypes.STRING
+		mature_ht_unit: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
+		},
+		mature_sprd_val: {
+			type: DataTypes.DECIMAL(10,1)
+		},
+		mature_sprd_unit: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
+		},
+		soil_acidity_min: {
+			type: DataTypes.DECIMAL(10,1)
+		},
+		soil_acidity_max: {
+			type: DataTypes.DECIMAL(10,1)
+		},
+		water_req: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
 		soil_type: {
-			type: DataTypes.STRING
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		soil_pH: {
-			type: DataTypes.DECIMAL
+		sun_req: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		sow_depth: {
-			type: DataTypes.DECIMAL
+		sow_direct: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		temp_germ: {
-			type: DataTypes.STRING
+		sowing_depth_val: {
+			type: DataTypes.DECIMAL(10,1)
 		},
-		temp_grow: {
-			type: DataTypes.STRING
+		sowing_depth_unit: {
+			type: DataTypes.STRING,
+			defaultValue: "N/A"
 		},
-		growing: {
-			type: DataTypes.TEXT
+		tempF_grow_min: {
+			type: DataTypes.DECIMAL(10,1)
 		},
-		days_to_germ: {
-			type: DataTypes.DECIMAL
-		},
-		days_to_transplant: {
-			type: DataTypes.DECIMAL
-		},
-		days_to_maturity: {
-			type: DataTypes.DECIMAL
-		},
-		harvesting: {
-			type: DataTypes.TEXT
+		tempF_grow_max: {
+			type: DataTypes.DECIMAL(10,1)
 		}
-	});
+	},
+    {
+      timestamps: false
+    });
 	return Plant;
 };
