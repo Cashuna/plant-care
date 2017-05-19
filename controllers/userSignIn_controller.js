cabd983091d948a06eb606722cad7a29208c05ca
C Marshall <Cashuna@users.noreply.github.com>
2017-05-19 03:33:05 -0400

    user login/create renders.

--- controllers/userSignIn_controller.js
+++ controllers/userSignIn_controller.js
@@ -1,5 +1,5 @@
 // *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// userSignIn.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
 
 // Dependencies
