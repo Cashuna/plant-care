591d384797bf466706dafc8dd0e62b275c8c18d8
C Marshall <Cashuna@users.noreply.github.com>
2017-05-20 00:23:28 -0400

    sending cookie/

--- controllers/userSignIn_controller.js
+++ controllers/userSignIn_controller.js
@@ -4,22 +4,21 @@
 
 // Dependencies
 // ==================================================================
-
 // Requiring our models
-var db = require("../models"), bcrypt = require('bcrypt');
-var express = require('express'), userRoutes = express.Router();
+var db = require("../models"), bcrypt = require("bcrypt");
+var express = require("express"), userRoutes = express.Router();
 var jwt = require("jsonwebtoken");
 var salt = "$2a$10$BMaZfkUboe3WS0TGkvmpOu"; //TODO: create process.env. variable
 
 // Routes
 //=======================BEGIN LOGIN=================================
 userRoutes.get("/login", function (req, res) {
-    res.render('login', {
+    res.render("login", {
         status: "Welcome, login"
     });
 });
 
- userRoutes.post("/login", function(req, res) {
+ userRoutes.post("/login", function(req, res, next) {
      var passwordAttempt = req.body.password;
      db.signIn.findOne({
          where: {
@@ -27,37 +26,41 @@ userRoutes.get("/login", function (req, res) {
          }
      }).then(function(user) {
          if (!user) {
-             console.log('no user found');
-             res.render("newuser", {'status': 'You should create an account'});
+             console.log("no user found");
+             res.render("newuser", {"status": "You should create an account"});
          }
              //TODO: add max login attempts
          else {
              bcrypt.compare(passwordAttempt, user.password, function(err, valid) {
                  if (err || !valid) {
                      res.render("newuser", {
-                         'status' : 'Invalid username or password'
+                         "status" : "Invalid username or password"
                      });
                  }
                  else {
                      var userToken = jwt.sign({
                          exp: Math.floor(Date.now() / 1000) + (60 * 60),
                          data: user.id
-                     }, 'secretJWTsigningAndItsRandom');
+                     }, "secretJWTsigningAndItsRandom");
+                     res.cookie("userToken", userToken, {
+                         secure: process.env.NODE_ENV === "production",
+                         signed: true
+                     });
                      res.redirect("/dashboard");
-                     /*res.status(200).json({
-                         id: user.id,
-                         username: user.username,
-                         token: userToken
-                    });*/
+                        /*res.status(200).json({
+                            id: user.id,
+                            username: user.username,
+                            token: userToken
+                        });*/
                  }
              });
          }
-     });
- });
+     }).catch(next);
+ });//embed token in cookie
 
 //=======================BEGIN NEW USER===============================
 userRoutes.get("/newuser", function (req, res) {
-    res.render('newuser', {
+    res.render("newuser", {
         status: "Welcome, sign up."
     });
 });
@@ -70,9 +73,9 @@ userRoutes.post("/newuser", function(req, res) {
             username: req.body.username,
             password: hash
         }).then(function(dbPost) {
-            res.redirect("/auth/logIn"); //this may be "login"
+            res.redirect("/auth/login"); //this may be "login"
         }).catch(function(err) {
-            res.status(400).json({status: "invalid username or password"});
+            res.status(400).json({status: "invalid username or password", error: err});
         })
     });
 });
