15342d428a5d06053b81543064742a9d4fff1e4d
Nicole <nmarielockwood@gmail.com>
2017-08-06 21:06:08 -0400

    adding redirect with message for unavailable new username

--- controllers/userSignIn_controller.js
+++ controllers/userSignIn_controller.js
@@ -14,7 +14,7 @@ var salt = "$2a$10$BMaZfkUboe3WS0TGkvmpOu"; //TODO: create process.env. variable
 //=======================BEGIN LOGIN=================================
 userRoutes.get("/login", function (req, res) {
     res.render("login", {
-        status: "Welcome, login"
+        status: "Welcome! Login below."
     });
 });
 
@@ -27,14 +27,14 @@ userRoutes.post("/login", function(req, res, next) {
      }).then(function(user) {
          if (!user) {
              console.log("no user found");
-             res.render("newuser", {"status": "You should create an account"});
+             res.render("newuser", {"status": "Looks like you're a new user - create an account here."});
          }
              //TODO: add max login attempts
          else {
              bcrypt.compare(passwordAttempt, user.password, function(err, valid) {
                  if (err || !valid) {
                      res.render("newuser", {
-                         "status" : "Invalid username or password"
+                         "status" : "Invalid username or password. Need a new account? Create one here."
                      });
                  }
                  else {
@@ -61,22 +61,32 @@ userRoutes.post("/login", function(req, res, next) {
 //=======================BEGIN NEW USER===============================
 userRoutes.get("/newuser", function (req, res) {
     res.render("newuser", {
-        status: "Welcome, sign up."
+        status: "Welcome! Sign-up below."
     });
 });
 //console.log(bcrypt.genSaltSnyc(10));
 // POST route for creating a new user
 userRoutes.post("/newuser", function(req, res) {
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-        // Store hash in your password DB.
-        db.signIn.create({
-            username: req.body.username,
-            password: hash
-        }).then(function(dbPost) {
-            res.redirect("/auth/login"); //this may be "login"
-        }).catch(function(err) {
-            res.status(400).json({status: "invalid username or password", error: err});
-        })
+    db.signIn.findOne({
+         where: {
+             username: req.body.username
+         }
+     }).then(function(user) {
+        if (user) {
+            console.log("username already exists.");
+            res.render("newuser", {"status": "Username already taken. Please choose a different username."});
+        }
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+            // Store hash in your password DB.
+            db.signIn.create({
+                username: req.body.username,
+                password: hash
+            }).then(function(dbPost) {
+                res.redirect("/auth/login"); //this may be "login"
+            }).catch(function(err) {
+                res.status(400).json({status: "invalid username or password", error: err});
+            })
+        });
     });
 });
 
